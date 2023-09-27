279d510e20d64678bf895d9126e26b136a5a9cf7
James Turner <james.c.turner01@gmail.com>
2023-09-27 19:27:50 +0100

    populateTeams.js update

--- seeds/populateTeams.js
+++ seeds/populateTeams.js
@@ -1,3 +1,12 @@
+// if we are not in production mode, require the dotenv module
+// process.env.NODE_ENV is an environment variable that is usually development or production
+// if we are running in development mode require thedotenv package
+// it will hten take the variables we have defined in our .env file and add them int process.env in our node app
+// so we can access them in our files
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 //require the Team model
 const Team = require('../models/team') 
@@ -6,8 +15,8 @@ const Team = require('../models/team')
 const teams = require('./teams');
 
 //connect to database
-// 'mongodb://127.0.0.1:/yelp-camp'
-mongoose.connect('mongodb://127.0.0.1:/eckc-app');
+// 'mongodb://127.0.0.1:/eckc-app'
+mongoose.connect(process.env.DB_URL);
 
 //error handling for connecting to a database
 const db = mongoose.connection;
